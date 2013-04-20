ef24df59c439c7b4e199152489210101b4559740
Igor Rodionov <goruha@gmail.com>
2013-04-20 12:32:30 +0700

    change way to get if site installed

--- build/templates/drush-install.sh
+++ build/templates/drush-install.sh
@@ -15,7 +15,7 @@ DRUPAL_SITES_SUBDIR="${drupal.site.subdir}"
 DRUPAL_PROFILE="${project.drupal.profile}"
 DRUPAL_ENVIRONMENT="${env.environment}"
 
-${DRUSH_BINARY} status --root="${DRUPAL_ROOT}" > /dev/null 2>&1
+${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" vset maintenance_mode 1 > /dev/null 2>&1
 
 if [ "$?" == "0" ]; then
 	echo "site installed"
@@ -23,12 +23,11 @@ else
 	echo "site not installed"
   commands[1] = '${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --account-name="${DRUPAL_ACC_NAME}" --account-pass="${DRUPAL_ACC_PASS}" --account-mail="${DRUPAL_ACC_MAIL}" --locale="${DRUPAL_LOCALE}" --clean-url="${DRUPAL_CLEAN_URL}" --site-name="${DRUPAL_SITE_NAME}" --site-mail="${DRUPAL_SITE_MAIL}" --sites-subdir="${DRUPAL_SITES_SUBDIR}" --yes site-install ${DRUPAL_PROFILE} install_configure_form.locale="${DRUPAL_LOCALE}"'
 fi
-commands[2]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" vset maintenance_mode 1'
-commands[3]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" --yes cache-clear drush'
-commands[4]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" --yes solution_install'
-commands[5]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" --yes --force --strict=0 environment-switch ${DRUPAL_ENVIRONMENT}'
-commands[6]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" --yes cache-clear all'
-commands[7]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" vset maintenance_mode 0'
+commands[2]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" --yes cache-clear drush'
+commands[3]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" --yes solution_install'
+commands[4]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" --yes --force --strict=0 environment-switch ${DRUPAL_ENVIRONMENT}'
+commands[5]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" --yes cache-clear all'
+commands[6]='${DRUSH_BINARY} --root="${DRUPAL_ROOT}" --user="1" vset maintenance_mode 0'
 
 for cmd in ${!commands[*]}
 do
